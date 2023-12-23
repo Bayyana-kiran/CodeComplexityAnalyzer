9bd0754243da1d852683d6808a6b8c50f47500bc
Bayyana-kiran <99533113+Bayyana-kiran@users.noreply.github.com>
2023-12-23 15:21:24 +0530

    Intial Commit

---
+++ javascript/script.js
@@ -0,0 +1,50 @@
+//function hello()
+//{
+//     // if (true)
+//     {
+//         var a="var";
+//         let b="let";
+//         const c="const";
+//         console.log(a);
+//         console.log(b);
+//         console.log(c);
+//     }
+// }
+// hello()
+//converting data type:
+//let x="23"
+//console.log(typeof x)
+// //${variable_name}
+// let a=19
+// let b=10
+// if(a>b)
+// {
+//     console.log("AIUSBC")
+// }
+// else
+// {
+//     console.log("oaisugd")
+// }
+// let age=89
+// let a=age>18?"akud" : "aisudg"
+// // console.log(a);
+// function hello()
+// {
+//     let a=5
+//     let b=10;
+//     console.log(a+b)
+
+// }
+// hello()
+//arguments and parameters and return
+// function sample()
+// {
+//     return 98
+// }
+// let ans=sample()
+// console.log(ans)
+var a=function(b)
+{
+    return 10 + b
+}
+console.log(a(10))
